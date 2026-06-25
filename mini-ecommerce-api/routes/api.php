f5a1c5078fb35ac8fb86d883937e3c4c74bcb06e
muhammad saifuddin <elaina480p@gmail.com>
2026-06-25 17:16:10 +0700

    feat: initial local setup for decoupling project

---
+++ mini-ecommerce-api/routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/produk', function (){
+    return response()->json([
+        'status' => 'success',
+        'message' => 'data produk berhasil di ambil dari be laravel!',
+        'data' => [
+            ['id' => 1,'nama' => 'sepatu sigma', 'harga' => 150000],
+            ['id' => 2,'nama' => 'tas sekolah', 'harga' => 75000],
+            ['id' => 3,'nama' => 'kotak bekal', 'harga' => 30000],
+            ['id' => 4,'nama' => 'buku catatan', 'harga' => 5000]
+        ]
+    ]);
+});
